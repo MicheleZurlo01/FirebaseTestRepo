544c19ba6465f185104b7e7bc5e74787124e3912
MikeZ2001 <mi.zurlo@studenti.unina.it>
2022-11-19 14:59:27 +0100

    Update LoginActivity.java
    -Cambio tipologia del login, ora è possibile accedere con One Tap Login (Ultima versione di login by Google)

--- app/src/main/java/com/example/myapplication/LoginActivity.java
+++ app/src/main/java/com/example/myapplication/LoginActivity.java
@@ -8,6 +8,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 
 import android.content.Intent;
+import android.content.IntentSender;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
@@ -17,29 +18,32 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 
+import com.google.android.gms.auth.api.identity.BeginSignInRequest;
+import com.google.android.gms.auth.api.identity.BeginSignInResult;
+import com.google.android.gms.auth.api.identity.Identity;
+import com.google.android.gms.auth.api.identity.SignInClient;
+import com.google.android.gms.auth.api.identity.SignInCredential;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.CommonStatusCodes;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
-import com.google.firebase.auth.internal.zzx;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.FirebaseDatabase;
-
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
 
 
 public class LoginActivity extends AppCompatActivity {
 
+    private static final int REQ_ONE_TAP = 0;
+    private boolean showOneTapUI = true;
     private LoginController controller;
 
     private Button registraAccountButton;
@@ -58,6 +62,9 @@ public class LoginActivity extends AppCompatActivity {
     private GoogleSignInClient mGoogleSignInClient;
     private final static int RC_SIGN_IN = 123;
 
+    private SignInClient oneTapClient;
+    private BeginSignInRequest.Builder signInRequest;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -72,7 +79,7 @@ public class LoginActivity extends AppCompatActivity {
         firebaseAuth = FirebaseAuth.getInstance();
 
 
-        //Automatic Login Firebase Credentials
+        //Automatic Login Firebase Credentials //Works for Firebase Login
         if(firebaseAuth.getCurrentUser() != null){
             controller.apriMainActivity();
         }
@@ -84,11 +91,14 @@ public class LoginActivity extends AppCompatActivity {
     protected void onStart() {
         super.onStart();
 
+       //Works for Google Login
         FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
 
         if(firebaseUser != null){
             controller.apriMainActivity();
         }
+
+
     }
 
     private void ottieniRiferimenti(){
@@ -104,7 +114,46 @@ public class LoginActivity extends AppCompatActivity {
         configuraAccediButton();
         configuraRegistraButton();
         //configuraAccediFacebookButton();
+
+
         configuraAccediGoogleButton();
+
+    }
+
+    private void accediConOneTapClient() {
+
+        oneTapClient = Identity.getSignInClient(this);
+        signInRequest = BeginSignInRequest.builder()
+                .setGoogleIdTokenRequestOptions(BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
+                        .setSupported(true)
+                        // Your server's client ID, not your Android client ID.
+                        .setServerClientId(getString(R.string.default_web_client_id))
+                        // Only show accounts previously used to sign in.
+                        .setFilterByAuthorizedAccounts(true)
+                        .build());
+
+        oneTapClient.beginSignIn(signInRequest.build())
+                .addOnSuccessListener(this, new OnSuccessListener<BeginSignInResult>() {
+                    @Override
+                    public void onSuccess(BeginSignInResult result) {
+                        try {
+                            int REQ_ONE_TAP = 0;
+                            startIntentSenderForResult(
+                                    result.getPendingIntent().getIntentSender(), REQ_ONE_TAP,
+                                    null, 0, 0, 0);
+                        } catch (IntentSender.SendIntentException e) {
+                            Log.e(TAG, "Couldn't start One Tap UI: " + e.getLocalizedMessage());
+                        }
+                    }
+                })
+                .addOnFailureListener(this, new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        // No saved credentials found. Launch the One Tap sign-up flow, or
+                        // do nothing and continue presenting the signed-out UI.
+                        Log.d(TAG, e.getLocalizedMessage());
+                    }
+                });
     }
 
     private void configuraAccediButton(){
@@ -118,6 +167,7 @@ public class LoginActivity extends AppCompatActivity {
 
     private void configuraAccediGoogleButton(){
 
+/*     OLD CODE FOR SIGN IN WITH GOOGLE
 
         //Configure Google Sign In
         GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
@@ -128,24 +178,39 @@ public class LoginActivity extends AppCompatActivity {
         //Build a sign in client with the google sign in option client
         mGoogleSignInClient = GoogleSignIn.getClient(this, googleSignInOptions);
 
+ */
+
         accediGoogleButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                signIn();
+
+                accediConOneTapClient();
+
+                //Old code for sign in with google
+               // signIn();
 
             }
         });
     }
 
+    /*    OLD CODE FOR SIGN IN WITH GOOGLE
     private void signIn(){
+
         Intent signInIntent = mGoogleSignInClient.getSignInIntent();
         startActivityForResult(signInIntent,RC_SIGN_IN);
+
+
+
     }
+    */
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+        /*    OLD CODE FOR SIGN IN WITH GOOGLE
+
+
         // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent
         if (requestCode == RC_SIGN_IN) {
             Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
@@ -160,7 +225,71 @@ public class LoginActivity extends AppCompatActivity {
                 Log.w(TAG, "Google sign in failed", e);
             }
         }
+
+         */
+
+
+        switch (requestCode) {
+            case REQ_ONE_TAP:
+
+
+                    SignInCredential googleCredential = null;
+                    try {
+                        googleCredential = oneTapClient.getSignInCredentialFromIntent(data);
+
+                    String idToken = googleCredential.getGoogleIdToken();
+                    if (idToken !=  null) {
+                        // Got an ID token from Google. Use it to authenticate
+                        // with Firebase.
+                        AuthCredential firebaseCredential = GoogleAuthProvider.getCredential(idToken, null);
+                        firebaseAuth.signInWithCredential(firebaseCredential)
+                                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<AuthResult> task) {
+                                        if (task.isSuccessful()) {
+                                            // Sign in success, update UI with the signed-in user's information
+                                            Log.d(TAG, "signInWithCredential:success");
+                                            FirebaseUser user = firebaseAuth.getCurrentUser();
+
+                                            //Print of the user
+                                            System.out.println("Google login "+user.getDisplayName());
+
+                                            controller.apriMainActivity();
+                                            //  updateUI(user);
+                                        } else {
+                                            // If sign in fails, display a message to the user.
+                                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                                            // updateUI(null);
+                                        }
+                                    }
+                                });
+                    }
+
+                } catch (ApiException e) {
+                    switch (e.getStatusCode()) {
+                        case CommonStatusCodes.CANCELED:
+                            Log.d(TAG, "One-tap dialog was closed.");
+                            // Don't re-prompt the user.
+                            showOneTapUI = false;
+                            break;
+                        case CommonStatusCodes.NETWORK_ERROR:
+                            Log.d(TAG, "One-tap encountered a network error.");
+                            // Try again or just ignore.
+                            break;
+                        default:
+                            Log.d(TAG, "Couldn't get credential from result."
+                                    + e.getLocalizedMessage());
+                            break;
+                    }
+                }
+                break;
+        }
+
+
     }
+
+    /*       OLD CODE FOR SIGN IN WITH GOOGLE
+
 
     private void firebaseAuthWithGoogle(GoogleSignInAccount account) {
 
@@ -188,6 +317,8 @@ public class LoginActivity extends AppCompatActivity {
                 });
 
     }
+
+     */
 
     private void configuraRegistraButton(){
         registraAccountButton.setOnClickListener(new View.OnClickListener() {
@@ -234,12 +365,5 @@ public class LoginActivity extends AppCompatActivity {
         return false;
     }
 
-    /*
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        callbackManager.onActivityResult(requestCode, resultCode, data);
-        super.onActivityResult(requestCode, resultCode, data);
-    }
 
-     */
 }
